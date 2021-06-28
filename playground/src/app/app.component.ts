a91de766d2ac317206f4bb85b1efa6c429e58e20
Gerd Jungbluth <gerd.jungbluth@engawa.de>
2021-06-28 11:48:26 +0000

    chore(setup): initial project

---
+++ playground/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pe-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'playground';
+}
